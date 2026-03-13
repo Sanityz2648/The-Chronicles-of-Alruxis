06778860abe8ed6ebda6bc75149e07efd18df375
Sanityz2648 <letsgoagainyt@gmail.com>
2026-03-13 13:44:51 -0400

    Add sidebar structure to sidebar.js

---
+++ ChroniclesOfAlruxis/sidebar.js
@@ -0,0 +1,32 @@
+document.getElementById('sidebar-container').innerHTML = `
+            <div class="sidebar">
+                <a href="index.html" class="sidebar-title">
+                    <h2>Chronicles of Alruxis</h2>
+                </a>
+
+                <details class="nav-section">
+                    <summary class="nav-header">Owners of Creation</summary>
+                    <ul class="nav-list">
+                        <li><a href="allfather.html">The Allfather</a></li>
+                    </ul>
+                </details>
+
+                <details class="nav-section">
+                    <summary class="nav-header">The Primordials</summary>
+                    <ul class="nav-list">
+                        <li><a href="#sevreth">Sevreth</a></li>
+                        <li><a href="#sorvaine">Sorvaine</a></li>
+                        <li><a href="#vaidreth">Vaidreth</a></li>
+                        <li><a href="#edrevos">Edrevos</a></li>
+                        <li><a href="#vorakin">Vorakin</a></li>
+                        <li><a href="#aravori">Aravori</a></li>
+                        <li><a href="#sevarus">Sevarus</a></li>
+                        <li><a href="#thessari">Thessari</a></li>
+                        <li><a href="#gorvokul">Gorvokul</a></li>
+                        <li><a href="#zelvari">Zelvari</a></li>
+                        <li><a href="#vorathis">Vorathis</a></li>
+                        <li><a href="#neroval">Neroval</a></li>
+                    </ul>
+                </details>
+            </div>
+`;
